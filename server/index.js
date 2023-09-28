f3be56afcaca94bbad1e8b9527f45f31eb6e471a
Cristian <creature.secret@gmail.com>
2023-09-28 18:16:13 -0300

    Se añade el puerto de la base de datos

--- server/index.js
+++ server/index.js
@@ -2,18 +2,11 @@ import 'dotenv/config';
 import { sequelize } from './src/database/db.js';
 import app from './src/app.js';
 
-import './src/models/User.js';
-import './src/models/Exercise.js';
-import './src/models/Follow.js';
-import './src/models/Like_post.js';
-import './src/models/Post.js';
-import './src/models/Training.js';
-
 const {PORT} = process.env;
 
 const main = async () => {
   try {
-    await sequelize.sync({force: true});
+    await sequelize.sync({force: false});
     console.log('Connection has been established succesfully.');
     // Se conecta al servidor
     app.listen(PORT, () => {
--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,7 +1,8 @@
-import { Sequelize } from "sequelize";
-const { DB_USER, DB_NAME, DB_PASSW, DB_HOST } = process.env;
+import { Sequelize } from 'sequelize';
+const { DB_USER, DB_NAME, DB_PASSW, DB_HOST, DB_PORT } = process.env;
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSW, {
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: DB_PORT
 });
